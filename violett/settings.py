c381f3909eb027709fc4287b355dfa9bb4afc714
swethaep99 <85289802+swethaep99@users.noreply.github.com>
2024-06-04 12:14:57 +0530

    email settings

--- violett/settings.py
+++ violett/settings.py
@@ -156,12 +156,12 @@ FILE_UPLOAD_HANDLERS = [
 # EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
-EMAIL_BACKEND =  'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST =  'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_BACKEND =  config('EMAIL_BACKEND')
+EMAIL_HOST =  config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER =  'violet.store.she@gmail.com'
-EMAIL_HOST_PASSWORD =  'dnir vhyp vada zxnu'
+EMAIL_HOST_USER =  config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD =  config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
  
 
